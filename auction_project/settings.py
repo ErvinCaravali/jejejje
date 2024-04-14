86583854b391fe237d4dd0a567c2f5acd03a4a97
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-14 01:36:23 -0500

    tyoo

--- auction_project/settings.py
+++ auction_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdi
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
