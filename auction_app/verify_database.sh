45c46f1e69cdea8f67261357729c520118a59732
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-16 15:48:25 -0500

    kfdñtkkggff

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -3,37 +3,25 @@
 # Crear la red si aún no existe
 docker network inspect auction_project_mi-red >/dev/null 2>&1 || docker network create auction_project_mi-red
 
-# Array que contiene los nombres de las tablas que se verificarán
-tables=("auctions" "artworks" "customers" "bids" "admins")
-
-# Variable para controlar si las tablas están pobladas
-populated=true
-
 # Iniciar un contenedor temporal basado en la imagen especificada
-container_id=$(docker run -p 5433:5432 --name db --network auction_project_mi-red ervincaravaliibarra/bdgaleria-8:latest)
+docker run -d --rm --name db --network auction_project_mi-red ervincaravaliibarra/bdgaleria-8:latest
 
-# Iterar sobre cada tabla y verificar si está poblada
-for table in "${tables[@]}"; do
-    # Ejecutar consulta SQL para contar registros en la tabla actual
-    count=$(docker exec -i "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM $table;")
-    
-    # Imprimir el nombre de la tabla y la cantidad de registros
-    echo "Table $table has $count records."
-    
-    # Verificar si la tabla está vacía
-    if [[ "$count" -eq 0 ]]; then
-        populated=false
-    fi
-done
+# Consulta SQL para contar registros en cada tabla
+count_auctions=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM auctions;")
+count_artworks=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM artworks;")
+count_customers=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM customers;")
+count_bids=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM bids;")
+count_admins=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM admins;")
 
-# Detener y eliminar el contenedor temporal
-docker stop "$container_id"
-
-# Imprimir el resultado de la verificación
-if [ "$populated" = true ]; then
-    echo "All tables are populated."
-    exit 0  # Indica que la verificación fue exitosa
-else
+# Verificar si alguna tabla está vacía
+if [[ "$count_auctions" -eq 0 || "$count_artworks" -eq 0 || "$count_customers" -eq 0 || "$count_bids" -eq 0 || "$count_admins" -eq 0 ]]; then
     echo "One or more tables are not populated."
-    exit 1  # Indica que la verificación fall
+    # Detener y eliminar el contenedor temporal
+    docker stop db
+    exit 1
+else
+    echo "All tables are populated."
+    # Detener y eliminar el contenedor temporal
+    docker stop db
+    exit 0
 fi
