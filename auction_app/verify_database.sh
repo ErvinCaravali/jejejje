8e24ce341f5483077f1143d4c4a7ad62d46f5866
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-16 15:26:41 -0500

    kfdñtgg

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-tables=("Auctions" "Artworks" "Customers" "Bids" "Admins")
+# Array que contiene los nombres de las tablas que se verificarán
+tables=("auctions" "artworks" "customers" "bids" "admins")
+
+# Variable para controlar si las tablas están pobladas
 populated=true
 
 # Iniciar un contenedor temporal basado en la imagen especificada
-container_id=$(docker run -d --rm ervincaravaliibarra/bdgaleria-8:latest)
+container_id=$(docker run -p 5433:5432 --name db --network auction_project_mi-red ervincaravaliibarra/bdgaleria-8:latest)
 
+# Iterar sobre cada tabla y verificar si está poblada
 for table in "${tables[@]}"; do
+    # Ejecutar consulta SQL para contar registros en la tabla actual
     count=$(docker exec -i "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM $table;")
+    
+    # Imprimir el nombre de la tabla y la cantidad de registros
     echo "Table $table has $count records."
+    
+    # Verificar si la tabla está vacía
     if [[ "$count" -eq 0 ]]; then
         populated=false
     fi
@@ -17,8 +26,11 @@ done
 # Detener y eliminar el contenedor temporal
 docker stop "$container_id"
 
+# Imprimir el resultado de la verificación
 if [ "$populated" = true ]; then
-    echo "true"
+    echo "All tables are populated."
+    exit 0  # Indica que la verificación fue exitosa
 else
-    echo "false"
+    echo "One or more tables are not populated."
+    exit 1  # Indica que la verificación falló
 fi
