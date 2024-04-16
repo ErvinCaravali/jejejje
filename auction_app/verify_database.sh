41e831afc959d4dbd195197ad9d0c3202fe6983a
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-16 15:53:46 -0500

    kfdñtkkggffjk

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -4,24 +4,24 @@
 docker network inspect auction_project_mi-red >/dev/null 2>&1 || docker network create auction_project_mi-red
 
 # Iniciar un contenedor temporal basado en la imagen especificada
-docker run -d --rm --name db --network auction_project_mi-red ervincaravaliibarra/bdgaleria-8:latest
+container_id=$(docker run -d --rm --name db --network auction_project_mi-red ervincaravaliibarra/bdgaleria-8:latest)
 
 # Consulta SQL para contar registros en cada tabla
-count_auctions=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM auctions;")
-count_artworks=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM artworks;")
-count_customers=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM customers;")
-count_bids=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM bids;")
-count_admins=$(docker exec db psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM admins;")
+count_auctions=$(docker exec "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM auctions;")
+count_artworks=$(docker exec "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM artworks;")
+count_customers=$(docker exec "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM customers;")
+count_bids=$(docker exec "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM bids;")
+count_admins=$(docker exec "$container_id" psql -U postgres -d projecto -t -c "SELECT COUNT(*) FROM admins;")
 
 # Verificar si alguna tabla está vacía
 if [[ "$count_auctions" -eq 0 || "$count_artworks" -eq 0 || "$count_customers" -eq 0 || "$count_bids" -eq 0 || "$count_admins" -eq 0 ]]; then
     echo "One or more tables are not populated."
     # Detener y eliminar el contenedor temporal
-    docker stop db
+    docker stop "$container_id"
     exit 1
 else
     echo "All tables are populated."
     # Detener y eliminar el contenedor temporal
-    docker stop db
+    docker stop "$container_id"
     exit 0
 fi
