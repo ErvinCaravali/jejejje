d09371e0f2a400fd3d7db8fd5dd693a5c49aee98
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-16 23:59:38 -0500

    wk

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -7,14 +7,23 @@ DB_NAME="projecto"
 DB_USER="postgres"
 DB_PASSWORD="Ec94"
 
-# Verificar si las tablas existen en la base de datos
+# Función para verificar si las tablas existen en la base de datos
 tables_exist() {
     psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c "\dt $1" | grep -q "No matching relations found"
     return $?
 }
 
-# Verificar la existencia de las tablas específicas
-if tables_exist "auctions" && tables_exist "artworks" && tables_exist "customers" && tables_exist "bids" && tables_exist "admins"; then
+# Esperar a que el servicio de base de datos esté disponible
+wait_for_db() {
+    while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER > /dev/null 2>&1; do
+        echo "Esperando que el servicio de base de datos esté disponible..."
+        sleep 10
+    done
+}
+
+# Verificar la existencia de las tablas específicas después de esperar a que la base de datos esté disponible
+wait_for_db
+if tables_exist "tabla1" && tables_exist "tabla2"; then
     echo "Las tablas requeridas existen en la base de datos."
     exit 0
 else
