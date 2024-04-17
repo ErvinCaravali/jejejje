22e621007a6cfeb0c1fa2b1b9832b0ee209b8db2
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-17 12:24:45 -0500

    ccfldfygfddecccfffgggkffo

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Función para verificar si las tablas existen en la base de datos
-tables_exist() {
-    psql -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER -d $DATABASE_NAME -c "\dt $1" | grep -q "No matching relations found"
-    return $?
-}
+# Define las consultas SQL para verificar si las tablas están vacías
+AUCTIONS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM auctions) THEN '' ELSE 'Advertencia: La tabla auctions está vacía' END;"
+ARTWORKS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM artworks) THEN '' ELSE 'Advertencia: La tabla artworks está vacía' END;"
+CUSTOMERS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM customers) THEN '' ELSE 'Advertencia: La tabla customers está vacía' END;"
+BIDS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM bids) THEN '' ELSE 'Advertencia: La tabla bids está vacía' END;"
+ADMINS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM admins) THEN '' ELSE 'Advertencia: La tabla admins está vacía' END;"
 
-# Esperar a que el servicio de base de datos esté disponible
-wait_for_db() {
-    while ! psql -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USER -c "SELECT 1;" > /dev/null 2>&1; do
-        echo "Esperando que el servicio de base de datos esté disponible..."
-        sleep 1
-    done
-}
-
-# Verificar la existencia de las tablas específicas después de esperar a que la base de datos esté disponible
-wait_for_db
-if tables_exist "tabla1" && tables_exist "tabla2"; then
-    echo "Las tablas requeridas existen en la base de datos."
-    exit 0
-else
-    echo "Una o más tablas requeridas no existen en la base de datos."
-    exit 1
-fi
+# Ejecuta las consultas SQL y muestra los resultados
+echo "Verificación de tablas en la base de datos:"
+echo "------------------------------------------"
+echo "Tabla auctions: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c \"$AUCTIONS_QUERY\")"
+echo "Tabla artworks: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c \"$ARTWORKS_QUERY\")"
+echo "Tabla customers: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c \"$CUSTOMERS_QUERY\")"
+echo "Tabla bids: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c \"$BIDS_QUERY\")"
+echo "Tabla admins: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c \"$ADMINS_QUERY\")"
