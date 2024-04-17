a00e0d4c355b22fd93b558a619854958c7e22322
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-17 17:23:35 -0500

    okrrffñlf

--- auction_app/verify_database.sh
+++ auction_app/verify_database.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export DATABASE_USER=postgres
-export DATABASE_HOST=172.20.0.2
-export DATABASE_NAME=projecto
-
 # Define las consultas SQL para verificar si las tablas están vacías
 AUCTIONS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM auctions) THEN '' ELSE 'Advertencia: La tabla auctions está vacía' END;"
 ARTWORKS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM artworks) THEN '' ELSE 'Advertencia: La tabla artworks está vacía' END;"
@@ -11,11 +7,11 @@ CUSTOMERS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM customers) THEN '' ELSE 
 BIDS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM bids) THEN '' ELSE 'Advertencia: La tabla bids está vacía' END;"
 ADMINS_QUERY="SELECT CASE WHEN EXISTS (SELECT 1 FROM admins) THEN '' ELSE 'Advertencia: La tabla admins está vacía' END;"
 
-# Ejecuta las consultas SQL y muestra los resultado
+# Ejecutar las consultas SQL y mostrar los resultados
 echo "Verificación de tablas en la base de datos:"
 echo "------------------------------------------"
-echo "Tabla auctions: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c "$AUCTIONS_QUERY")"
-echo "Tabla artworks: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c "$ARTWORKS_QUERY")"
-echo "Tabla customers: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c "$CUSTOMERS_QUERY")"
-echo "Tabla bids: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c "$BIDS_QUERY")"
-echo "Tabla admins: $(psql -U $DATABASE_USER -h $DATABASE_HOST -d $DATABASE_NAME -c "$ADMINS_QUERY")"
+echo "Tabla auctions: $(psql -U postgres -h 172.20.0.2 -d projecto -c \"$AUCTIONS_QUERY\")"
+echo "Tabla artworks: $(psql -U postgres -h 172.20.0.2 -d projecto -c \"$ARTWORKS_QUERY\")"
+echo "Tabla customers: $(psql -U postgres -h 172.20.0.2 -d projecto -c \"$CUSTOMERS_QUERY\")"
+echo "Tabla bids: $(psql -U postgres -h 172.20.0.2 -d projecto -c \"$BIDS_QUERY\")"
+echo "Tabla admins: $(psql -U postgres -h 172.20.0.2 -d projecto -c \"$ADMINS_QUERY\")"
